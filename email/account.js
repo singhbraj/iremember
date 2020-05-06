1fee8bb8f7362dc6c4598ece7d046a3697ba794b
singhbraj <42037630+singhbraj@users.noreply.github.com>
2020-05-06 21:15:13 +0530

    Update account.js

--- email/account.js
+++ email/account.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(API)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'manishkumar199222@gmail.com',
+        from: 'braj333singh@gmail.com',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -16,7 +16,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'manishkumar199222@gmail.com',
+        from: 'braj333singh@gmail.com',
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
